d6c1b5ea97cbab2fe34a1197e9c34ba65c993935
Marouane <MarouaneBenbetka@gmail.com>
2024-02-18 07:54:14 +0100

    feat: sql scripts + backup

---
+++ sql/ddl.sql
@@ -0,0 +1,112 @@
+-- DROP SCHEMA public CASCADE;
+-- CREATE SCHEMA public;
+-- GRANT ALL ON SCHEMA public TO postgres;
+-- GRANT ALL ON SCHEMA public TO public;
+CREATE TYPE sources_enum AS ENUM (
+    'Github',
+    'Kaggle',
+    'UCI-ML-Repo',
+    'Hugging Face'
+);
+CREATE TABLE IF NOT EXISTS sources (
+    id SERIAL PRIMARY KEY,
+    name sources_enum NOT NULL UNIQUE,
+    url VARCHAR(255),
+    description TEXT
+);
+CREATE TABLE IF NOT EXISTS licenses (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE,
+    url VARCHAR(255),
+    description TEXT
+);
+CREATE TABLE IF NOT EXISTS stats (
+    id SERIAL PRIMARY KEY,
+    views_count INT DEFAULT 0 CHECK (views_count >= 0),
+    likes_count INT DEFAULT 0 CHECK (likes_count >= 0),
+    download_count INT DEFAULT 0 CHECK (download_count >= 0)
+);
+CREATE TABLE IF NOT EXISTS owners (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    profile_url VARCHAR(255) UNIQUE
+);
+CREATE TABLE IF NOT EXISTS tags (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+CREATE TABLE IF NOT EXISTS use_cases (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE,
+    description TEXT
+);
+CREATE TABLE IF NOT EXISTS datasets (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL UNIQUE,
+    ref VARCHAR(255),
+    description TEXT,
+    total_bytes BIGINT CHECK (total_bytes >= 0),
+    source_id INT REFERENCES sources(id) ON DELETE CASCADE,
+    stats_id INT REFERENCES stats(id) ON DELETE CASCADE,
+    license_id INT REFERENCES licenses(id) ON DELETE
+    SET NULL
+);
+CREATE TABLE IF NOT EXISTS dataset_owners (
+    id SERIAL PRIMARY KEY,
+    owner_id INT REFERENCES owners(id) ON DELETE CASCADE,
+    dataset_id INT REFERENCES datasets(id) ON DELETE CASCADE,
+    UNIQUE (owner_id, dataset_id)
+);
+CREATE TABLE IF NOT EXISTS dataset_tags (
+    dataset_id INT REFERENCES datasets(id) ON DELETE CASCADE,
+    tag_id INT REFERENCES tags(id) ON DELETE CASCADE,
+    PRIMARY KEY (dataset_id, tag_id)
+);
+CREATE TABLE IF NOT EXISTS dataset_use_cases (
+    dataset_id INT REFERENCES datasets(id) ON DELETE CASCADE,
+    use_case_id INT REFERENCES use_cases(id) ON DELETE CASCADE,
+    PRIMARY KEY (dataset_id, use_case_id)
+);
+CREATE TABLE IF NOT EXISTS features (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    type TEXT,
+    description TEXT,
+    dataset_id INT REFERENCES datasets(id) ON DELETE CASCADE
+);
+CREATE TABLE notebooks (
+    id SERIAL PRIMARY KEY,
+    dataset_id INT REFERENCES datasets(id) ON DELETE CASCADE,
+    ref VARCHAR(255),
+    author VARCHAR(255),
+    last_run_time DATE,
+    total_votes INT DEFAULT 0 CHECK (total_votes >= 0),
+    language VARCHAR(255),
+    content JSON
+);
+CREATE TABLE IF NOT EXISTS issues (
+    id SERIAL PRIMARY KEY,
+    dataset_id INT REFERENCES datasets(id) ON DELETE CASCADE,
+    title VARCHAR(255),
+    body TEXT,
+    url VARCHAR(255),
+    creation_date DATE,
+    updated_date DATE,
+    reactions JSON,
+    comments JSON
+);
+CREATE TABLE IF NOT EXISTS discussions (
+    id SERIAL PRIMARY KEY,
+    dataset_id INT REFERENCES datasets(id) ON DELETE CASCADE,
+    title VARCHAR(255),
+    content TEXT,
+    url VARCHAR(255),
+    user_name VARCHAR(255),
+    comments JSON
+);
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    hashed_password TEXT NOT NULL
+);
---
+++ sql/views.sql
@@ -0,0 +1,56 @@
+-- View: Dataset Overview
+-- Provides a quick overview of datasets including their source, license, and basic statistics.
+CREATE VIEW dataset_overview AS
+SELECT d.id,
+    d.title,
+    s.name AS source,
+    l.name AS license,
+    st.views_count,
+    st.likes_count,
+    st.download_count
+FROM datasets d
+    JOIN sources s ON d.source_id = s.id
+    LEFT JOIN licenses l ON d.license_id = l.id
+    JOIN stats st ON d.stats_id = st.id;
+-- View: Dataset Tags and Use Cases
+-- Aggregates all tags and use cases associated with each dataset for easy filtering and searching.
+CREATE VIEW dataset_tags_use_cases AS
+SELECT d.id AS dataset_id,
+    d.title,
+    array_agg(t.name) AS tags,
+    array_agg(u.name) AS use_cases
+FROM datasets d
+    LEFT JOIN dataset_tags dt ON d.id = dt.dataset_id
+    LEFT JOIN tags t ON dt.tag_id = t.id
+    LEFT JOIN dataset_use_cases duc ON d.id = duc.dataset_id
+    LEFT JOIN use_cases u ON duc.use_case_id = u.id
+GROUP BY d.id;
+-- Materialized View: Most Popular Datasets
+-- Keeps a cached list of the most popular datasets based on views, likes, and downloads.
+-- Useful for reducing computation time for frequently accessed statistics on dataset popularity.
+CREATE MATERIALIZED VIEW most_popular_datasets AS
+SELECT d.id,
+    d.title,
+    s.name AS source,
+    st.views_count,
+    st.likes_count,
+    st.download_count
+FROM datasets d
+    JOIN stats st ON d.stats_id = st.id
+    JOIN sources s ON d.source_id = s.id
+ORDER BY st.views_count DESC,
+    st.likes_count DESC,
+    st.download_count DESC
+LIMIT 100;
+-- Materialized View: Dataset Details by Owner
+-- Aggregates datasets along with their owners for a comprehensive view that includes ownership information.
+-- Useful for enhancing data accessibility by associating datasets with their respective owners.
+CREATE MATERIALIZED VIEW dataset_details_by_owner AS
+SELECT o.name AS owner_name,
+    o.profile_url,
+    d.title,
+    d.url,
+    d.description
+FROM dataset_owners dow
+    JOIN owners o ON dow.owner_id = o.id
+    JOIN datasets d ON dow.dataset_id = d.id;
